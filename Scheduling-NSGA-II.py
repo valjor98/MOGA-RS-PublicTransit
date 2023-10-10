2878c1c8233184253ea3d247b1cc26ae20a707b2
valjor98 <valjor98@gmail.com>
2023-10-10 15:52:50 -0600

    Added the functions for a Binary Selection Tournament and the Crossover as well as added docstrings

--- Scheduling-NSGA-II.py
+++ Scheduling-NSGA-II.py
@@ -21,6 +21,17 @@ class Individual:
 
 
 def _generate_random_block(target_schedule, max_block_length):
+    """
+    Helper function to generate 
+    
+    Parameters:
+        - target_schedule: list of the times an ideal line should cover
+        - max_block_length: maximum length a single block should have
+
+    Returns:
+        - block: the set of stops a single bus makes
+
+    """
     # Randomly choose a starting point and a length for the block
     start_index = random.randint(0, len(target_schedule) - 1)
     block_length = random.randint(1, max_block_length)
@@ -35,23 +46,50 @@ def _generate_random_block(target_schedule, max_block_length):
     return block
 
 def _initialize_population(pop_size, target_schedule, max_blocks_per_individual, max_block_length):
+    """
+    Helper function to generate an initial population of individual solutions.
+
+    Parameters:
+        - pop_size: size of the desired population
+        - target_schedule: list of the times an ideal line should cover
+        - max_blocks_per_individual: maximum number of blocks an individual should have
+        - max_block_length: maximum length a single block should have
+    Returns:
+        - population: a list containing individual solutions
+    """
     population = []
     for _ in range(pop_size):
         individual_schedule = [_generate_random_block(target_schedule, max_block_length) for _ in range(random.randint(1, max_blocks_per_individual))]
         population.append(Individual(individual_schedule))
     return population
 
-"""
-# Parameters
-pop_size = 100
-max_blocks_per_individual = 10  # Maximum number of blocks an individual can have
-max_block_length = 5  # Maximum length of each block
+def BinarySelectionTournament(population):
+    offspring = []
+    while len(offspring) < len(population):
+        candidates = random.sample(population, 2)
+        if candidates[0].front < candidates[1].front:
+            winner = candidates[0]
+        elif candidates[0].front > candidates[1].front:
+            winner = candidates[1]
+        else:
+            winner = candidates[0] if candidates[0].crowding_distance > candidates[1].crowding_distance else candidates[1]
+        offspring.append(winner)
+    return offspring
 
-population = initialize_population(pop_size, target_schedule, max_blocks_per_individual, max_block_length)
-"""
 
+def Crossover(parent1, parent2):
+    # Single point crossover for the starting times
+    crossover_point = random.randint(0, len(parent1.schedule))
+    child1_schedule = parent1.schedule[:crossover_point] + parent2.schedule[crossover_point:]
+    child2_schedule = parent2.schedule[:crossover_point] + parent1.schedule[crossover_point:]
+
+    # Create the child individuals and return
+    return Individual(child1_schedule), Individual(child2_schedule)
 
 def main():
+    """
+    Main execution of the NSGA-II algorithm for the scheduling problem.
+    """
     # Parameters
     pop_size = 100
     generations = 500
@@ -78,8 +116,8 @@ def main():
         for i in range(0, len(offspring) - 1, 2):  # ensuring we have pairs of offspring
             if random.random() < crossover_prob:
                 offspring[i], offspring[i + 1] = Crossover(offspring[i], offspring[i + 1])
-            Mutation(offspring[i], mutation_prob)
-            Mutation(offspring[i + 1], mutation_prob)
+            Mutation(offspring[i], mutation_prob, target_schedule, max_blocks_per_individual)
+            Mutation(offspring[i + 1], mutation_prob, target_schedule, max_blocks_per_individual)
 
         # Objective Function Evaluation for offspring
         for individual in offspring:
@@ -107,7 +145,6 @@ def main():
 
         population = next_gen_population
 
-    # Optionally, you can add code here to display or save the results.
 
 if __name__ == "__main__":
     main()
