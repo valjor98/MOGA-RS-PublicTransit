b467106c9ae513fcfe6077d2241de498a1231319
valjor98 <valjor98@gmail.com>
2023-10-10 13:31:15 -0600

    Added helpers functions for population initialization

--- Scheduling-NSGA-II.py
+++ Scheduling-NSGA-II.py
@@ -1,3 +1,5 @@
+import random
+
 class Individual:
     def __init__(self, blocks):
         self.blocks = blocks  # List of lists, where each sublist is a block of start times
@@ -16,3 +18,34 @@ class Individual:
         num_blocks = len(self.blocks)
 
         self.fitness = [uncovered_times, num_blocks]
+
+
+def _generate_random_block(target_schedule, max_block_length):
+    # Randomly choose a starting point and a length for the block
+    start_index = random.randint(0, len(target_schedule) - 1)
+    block_length = random.randint(1, max_block_length)
+    
+    # Extract the block of start times
+    block = target_schedule[start_index : start_index + block_length]
+    
+    # Handle wrapping around for cyclic schedules
+    if start_index + block_length > len(target_schedule):
+        block += target_schedule[:block_length - len(block)]
+    
+    return block
+
+def _initialize_population(pop_size, target_schedule, max_blocks_per_individual, max_block_length):
+    population = []
+    for _ in range(pop_size):
+        individual_schedule = [_generate_random_block(target_schedule, max_block_length) for _ in range(random.randint(1, max_blocks_per_individual))]
+        population.append(Individual(individual_schedule))
+    return population
+
+"""
+# Parameters
+pop_size = 100
+max_blocks_per_individual = 10  # Maximum number of blocks an individual can have
+max_block_length = 5  # Maximum length of each block
+
+population = initialize_population(pop_size, target_schedule, max_blocks_per_individual, max_block_length)
+"""
