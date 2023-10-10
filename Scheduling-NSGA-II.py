76e564fb5682dcbd8ad3eaefb1347640e2a7edad
valjor98 <valjor98@gmail.com>
2023-10-10 15:09:26 -0600

    Main structure fir the flow of the scheduler

--- Scheduling-NSGA-II.py
+++ Scheduling-NSGA-II.py
@@ -49,3 +49,65 @@ max_block_length = 5  # Maximum length of each block
 
 population = initialize_population(pop_size, target_schedule, max_blocks_per_individual, max_block_length)
 """
+
+
+def main():
+    # Parameters
+    pop_size = 100
+    generations = 500
+    crossover_prob = 0.9
+    mutation_prob = 0.1
+    max_blocks_per_individual = 10  # Maximum number of blocks an individual can have
+    max_block_length = 5  # Maximum length of each block
+
+    # Target Schedule
+    target_schedule = list(range(300, 1440, 15)) + list(range(0, 70, 15))
+
+    # Initialize the population
+    population = initialize_population(pop_size, target_schedule, max_blocks_per_individual, max_block_length)
+
+    # Evaluate objectives for the initial population
+    for individual in population:
+        individual.evaluate_objectives(target_schedule)
+    
+    for generation in range(generations):
+        # Selection
+        offspring = BinarySelectionTournament(population)
+
+        # Crossover and Mutation
+        for i in range(0, len(offspring) - 1, 2):  # ensuring we have pairs of offspring
+            if random.random() < crossover_prob:
+                offspring[i], offspring[i + 1] = Crossover(offspring[i], offspring[i + 1])
+            Mutation(offspring[i], mutation_prob)
+            Mutation(offspring[i + 1], mutation_prob)
+
+        # Objective Function Evaluation for offspring
+        for individual in offspring:
+            individual.evaluate_objectives(target_schedule)
+
+        # Combine parent and offspring populations
+        combined = population + offspring
+
+        # Fast non-dominated sort
+        fronts = FastNonDominatedSort(combined)
+
+        # Assign crowding distance
+        for front in fronts:
+            CrowdingDistance(front)
+
+        # Select the next generation based on Pareto front and crowding distance
+        next_gen_population = []
+        for front in fronts:
+            if len(next_gen_population) + len(front) <= len(population):
+                next_gen_population.extend(front)
+            else:
+                sorted_front = sorted(front, key=lambda x: (-x.front, -x.crowding_distance))  # select based on front and crowding distance
+                next_gen_population.extend(sorted_front[:len(population) - len(next_gen_population)])
+                break
+
+        population = next_gen_population
+
+    # Optionally, you can add code here to display or save the results.
+
+if __name__ == "__main__":
+    main()
