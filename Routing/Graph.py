9d0feec1478fa02d6a646569b0fa50ce6c69ae87
valjor98 <valjor98@gmail.com>
2023-11-14 21:50:16 -0600

    Added mapping function to get nodes from their id

--- Routing/Graph.py
+++ Routing/Graph.py
@@ -139,3 +139,17 @@ class Graph:
         for i, path in enumerate(paths):
             cost = self.path_cost(path)
             print(f"Path {i+1}: {path} with total cost {cost}")
+
+    def get_node(self, node_id):
+        """
+        Retrieve information for a list of node IDs
+
+        Parameters:
+            - node_ids: List of node IDs
+        
+        Return: 
+            - List of node objects or their attributes
+        """
+        node = self.nodes.get(node_id)
+
+        return node
